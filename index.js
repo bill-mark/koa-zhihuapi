ac45baeb7d04f2906c64a04d5be891092a1102ba
bill.mark <2286449127@qq.com>
2021-01-07 18:00:59 +0800

    增加kod-router

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
 
-app.use((ctx)=>{
-    ctx.body = 'hello body777 778899  kkll'
+const usersRouter = new Router({prefix:'/users'})
+
+app.use((ctx,next)=>{
+    console.log(ctx)
+    next()
 })
+
+const auth = async(ctx,next)=>{
+    console.log('auth')
+    if(ctx.url !== '/users'){
+        ctx.throw(401)
+    }
+    await next()
+}
+
+router.get('/',(ctx)=>{
+    ctx.body = 'this is page'
+})
+
+usersRouter.get('/', auth, (ctx)=>{
+    console.log(ctx.params.id)
+    ctx.body = 'this is users'
+})
+
+router.post('/users',(ctx)=>{
+    ctx.body = 'this is users post'
+})
+
+
+
+app.use(router.routes())
+app.use(usersRouter.routes()).use(usersRouter.allowedMethods())
 
 app.listen(3000)
